15810dce541b5099f802ede7ed804a5719e2b438
emirhanuzun <emirhanuzun98123@gmail.com>
2025-04-25 20:50:23 +0300

    javadocs added for main api

--- toyota-tcp-rate-api-platform/src/main/java/com/main/ClientConnection/ClientConnectionInterface.java
+++
@@ -1,8 +0,0 @@
-package com.main.ClientConnection;
-
-public interface ClientConnectionInterface {
-    void handleMessageTaken(String message);
-    void subscribeToRate(String rateName);
-    void unsubscribeFromRate(String rateName);
-    void sendSubscribedRates() throws InterruptedException;
-}
